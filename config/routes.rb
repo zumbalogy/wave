cbd72182e7c631ff7efc21b281c92f49a3d42ade
zumbalogy <intrapostal@gmail.com>
2014-02-26 13:36:25 -0500

    devise plugged in

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Final::Application.routes.draw do
 
   get '/' => 'main#index'
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
