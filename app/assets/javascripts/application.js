45dc7d377d81d6cbeb9dda7ecf9f68a6b5942b76
zumbalogy <intrapostal@gmail.com>
2014-02-26 19:13:18 -0500

    faye kinda working somewhat, boyaa

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,12 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+
+$(function(){
+    var faye = new Faye.Client("http://localhost:9292/faye")
+    // the below changes if you have multiple rooms
+    faye.subscribe('/messages/new', function(message){
+        alert(message)
+    })
+})
