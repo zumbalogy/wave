c92e282e1a469d7b04072215da3f44fba2137f2c
zumbalogy <intrapostal@gmail.com>
2014-03-01 11:19:42 -0500

    css and made some routes for carriers

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Final::Application.routes.draw do
   get "chats/room"
   root 'main#index'
   post '/convos/add' => 'convos#add'
-  devise_for :users
   resources :messages
+  resources :carriers
   resources :convos
+  devise_for :users
 
 
 end
